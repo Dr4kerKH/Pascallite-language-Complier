31c663c54413312b1db09141b675410efb40f46f
Sonpaorong M. <112592179+Dr4kerKH@users.noreply.github.com>
2025-02-18 17:39:07 -0600

    Add files via upload
    Test cases

---
+++ 002.asm
@@ -0,0 +1,8 @@
+; Sonpaorong Muchhim Sun Dec  1 16:14:18 2024
+%INCLUDE "Along32.inc"
+%INCLUDE "Macros_Along.inc"
+
+SECTION .text                           
+global  _start                          ; program stage0no002
+
+_start:                                 
